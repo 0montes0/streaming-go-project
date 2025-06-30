21b9fb0b885be10e3fd16aaece623ea177f7cf9b
0montes0 <andresmontes045@gmail.com>
2025-06-29 21:57:38 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
+package main
 
+import (
+    "log"
+    "net/http"
+    "github.com/gorilla/mux"
+)
+
+func main() {
+    r := mux.NewRouter()
+
+    // Rutas para servicios web
+    r.HandleFunc("/usuarios", GetUsuarios).Methods("GET")
+    r.HandleFunc("/usuarios", CrearUsuario).Methods("POST")
+    r.HandleFunc("/contenidos", GetContenidos).Methods("GET")
+    r.HandleFunc("/contenidos", CrearContenido).Methods("POST")
+    r.HandleFunc("/stream", StreamVideo).Methods("GET")
+    r.HandleFunc("/calificaciones", GetCalificaciones).Methods("GET")
+    r.HandleFunc("/calificaciones", CrearCalificacion).Methods("POST")
+
+    log.Println("Servidor corriendo en http://localhost:8080")
+    log.Fatal(http.ListenAndServe(":8080", r))
+}
